fdff9851bfa069ba5037e6e2e8a5408659f2c1e8
Madhusha Prasad <madushaprasad21@gmail.com>
2019-07-01 11:11:38 +0530

    Client Update

---
+++ Client/src/main/Connect.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import service.ServiceInterFace;
+
+
+
+/**
+ *
+ * @author madhusha
+ */
+public class Connect{
+    public static void main(String[] args) throws NotBoundException, MalformedURLException, RemoteException {
+        ServiceInterFace lookup = (ServiceInterFace) Naming.lookup("rmi:localhost:5050/POS1");
+        System.out.println("Hello Im Client");
+    }
+
+    
+    
+}
